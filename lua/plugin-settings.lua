ad7c3dfb6e894852adac87266a229e8f24807d49
kylehovey <khoveyc@gmail.com>
2023-05-05 14:20:07 -0700

    Organized plugin settings

--- lua/plugin-settings.lua
+++ lua/plugin-settings.lua
@@ -1,61 +1,37 @@
 -- Slime
--- We are using tmux as the slime recipient
 vim.g.slime_target = 'tmux'
--- Tmux slime config
 vim.g.slime_default_config = {
 	socket_name= "default",
 	target_pane="{left-of}"
 }
--- Don't ask to send by default
 vim.g.slime_dont_ask_default = 1
 
--- Julia
--- Use '##' tags to define cells
+-- Use '##' tags to define cells in Julia
 vim.g.julia_cell_delimit_cells_by = 'tags'
 
 -- Svelte
--- Enable Typescript
 vim.g.vim_svelte_plugin_use_typescript = 1
 
 -- LaTeX
--- Set Preview As the Default LaTeX Viewer
 vim.g.livepreview_previewer = 'open -a Preview'
--- Don't convert symbols
-vim.g.tex_conceal = ""
--- And please don't add text I didn't type...
-vim.g.Imap_FreezeImap = 1
--- Send errors to black hole
+vim.g.tex_conceal = "" -- Don't convert symbols
+vim.g.Imap_FreezeImap = 1 -- And please don't add text I didn't type...
 vim.g.Tex_ViewRule_pdf = 'evince 2>/dev/null'
--- Stop the linter and warnings
-vim.g.Tex_IgnoreLevel = 8
+vim.g.Tex_IgnoreLevel = 8 -- Stop the linter and warnings
 
 -- Markdown
--- Code Blocks
 _G.markdown_fenced_languages = {'coffee', 'css', 'erb=eruby', 'javascript', 'js=javascript', 'json=javascript', 'ruby', 'sass', 'xml', 'typescript', 'bash'}
 
 -- Versioning
--- Enable GitGutter by Default
-vim.g.gitgutter_enabled = 1
--- Use minimal menu for NerdTree
-vim.g.NERDTreeMinimalMenu = 1
+vim.g.gitgutter_enabled = 1 -- Enable GitGutter by Default
+vim.g.NERDTreeMinimalMenu = 1 -- Use minimal menu for NerdTree
 
--- JS
 -- Enable JSDoc syntax
 vim.g.javascript_plugin_jsdoc = 1
 
--- ACK/AG
--- Use ag instead of ack
-vim.g.ackprg = 'ag --vimgrep'
-
 -- Searching
--- Telescope
+vim.g.ackprg = 'ag --vimgrep' -- Use ag instead of ack
 require('telescope').load_extension "file_browser"
 require('telescope').setup({
-	defaults = {
-		layout_config = {
-			vertical = {
-				width = 0.5
-			}
-		}
-	}
+	defaults = { layout_config = { vertical = { width = 0.5 }}}
 })
